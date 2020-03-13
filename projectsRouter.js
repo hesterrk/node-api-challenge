352bd3cd452bb9f9475fbabea3160c7f415a3943
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-03-13 08:44:17 +0000

    converted to async

--- projectsRouter.js
+++ projectsRouter.js
@@ -1,36 +1,36 @@
-const express = require('express');
+const express = require("express");
 const projects = require("./data/helpers/projectModel");
 const router = express.Router();
 
-
 //GET All Projects
 
-router.get("/", (req, res, next) => {
-    projects
-      .get()
-      .then(project => {
-          if(project) {
-        res.status(200).json(project);
-          } else {
-              res.status(404).json({ message: "list of projects not here! "})
-          }
-      })
-      .catch(err => {
-        next(err);
-      });
-  });
+// router.get("/", (req, res, next) => {
+//     projects
+//       .get()
+//       .then(project => {
+//           if(project) {
+//         res.status(200).json(project);
+//           } else {
+//               res.status(404).json({ message: "list of projects not here! "})
+//           }
+//       })
+//       .catch(err => {
+//         next(err);
+//       });
+//   });
 
-
-
-
-
-
-
-
-
-
-
-
+router.get("/", async (req, res, next) => {
+  try {
+    const project = await projects.get();
+    if (project) {
+      res.status(200).json(project);
+    } else {
+      res.status(404).json({ message: "list of projects not here! " });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 
 
