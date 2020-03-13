d893b5582847e7d73541013a833c1901104c7c8b
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-03-13 10:10:18 +0000

    set up react app, installed axios

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  return (
+    <div className="App">
+      <h2> List of Your Projects </h2>
+     
+    </div>
+  );
+}
+
+export default App;
