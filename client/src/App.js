dae193df0e5b3640f4156cd1046d9e5928fc422d
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-03-13 10:24:14 +0000

    data from get projects displaying

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,39 @@ import axios from 'axios';
 
 function App() {
 
-  const [projects, setProjects] = useState();
+  const [projects, setProjects] = useState([]);
 
   useEffect(() => {
-
     //only works if you have npm run server!
+    const getProjects = () => {
     axios.get('http://localhost:4000/api/projects')
     .then(res => {
       console.log(res);
       setProjects(res.data)
+      console.log(projects)
     })
     .catch(err => {
       console.log(err);
     })
+
+  }
+
+  getProjects();
+
   }, [])
 
   return (
     <div className="App">
       <h2> List of Your Projects </h2>
+     
+         {projects.map(project => (
+          <div>
+          <p>{project.name}</p>
+          <p>{project.completed}</p>
+          </div>
+          
+  ))} 
+     
 
 
      
