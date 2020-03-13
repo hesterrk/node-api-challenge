49c3acd038ea9572005235863d10784db4b2530c
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-03-13 10:17:09 +0000

    got axios get projects working

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 
 function App() {
+
+  const [projects, setProjects] = useState();
+
+  useEffect(() => {
+
+    //only works if you have npm run server!
+    axios.get('http://localhost:4000/api/projects')
+    .then(res => {
+      console.log(res);
+      setProjects(res.data)
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }, [])
+
   return (
     <div className="App">
       <h2> List of Your Projects </h2>
+
+
      
     </div>
   );
